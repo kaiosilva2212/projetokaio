065e47bab68a1a725a7e0e0973b737d473c3cd46
unknown <vitorisf56@gmail.com>
2024-11-13 21:16:25 -0300

    operacoes matematicas

---
+++ operacoes.py
@@ -0,0 +1,13 @@
+valor = int(input("digite um número"))
+
+dobro =valor *2
+triplo=valor *3
+quadrado=valor *4
+raiz=valor **(1/2)
+raiz_cubica=valor **(1/3)
+
+print(f"O dobro de {valor} é {dobro}")
+print(f"O triplo de {valor} é {triplo}")
+print(f"O quadrado de {valor} é {quadrado}")
+print(f"O raiz de {valor} é {raiz}")
+print(f"O raiz cubica de {valor} é {raiz_cubica}")
